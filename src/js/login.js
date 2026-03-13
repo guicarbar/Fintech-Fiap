fd85101f21eb57dc3a6a58bfced81a2f5c7fee1f
Guilherme Carvalho <162490839+guicarbar@users.noreply.github.com>
2026-03-12 22:20:20 -0300

    Fase4 - UpdateFinal (#11)

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,56 @@
+// control const
+const controlBtn = document.getElementById("btnControl")
+var controlActive = true
+
+const boxCadastro = document.getElementById("cadastrobox")
+const boxLogin = document.getElementById("boxlogin")
+
+
+const loginBlock = document.getElementById("btn-login-icon")
+const cadastroBlock = document.getElementById("btn-cadastro-icon")
+
+// go back da pagina
 const link = document.getElementById("goBackIcon")
 
+
+// change bloscks
+function changeBlock(block) {
+  if (block == "Login") {
+    // Buttons
+    loginBlock.classList.remove("d-none")
+    cadastroBlock.classList.add("d-none")
+
+    // Divs
+    boxCadastro.classList.remove("d-none")
+    boxLogin.classList.add("d-none")
+  } else if (block == "Cadastro") {
+    // Buttons
+    loginBlock.classList.add("d-none")
+    cadastroBlock.classList.remove("d-none")
+    
+    // Divs
+    boxCadastro.classList.add("d-none")
+    boxLogin.classList.remove("d-none")
+  }
+}
+
+// evente functiion to link
 link.addEventListener("click", () => {
   window.location.href = "../../index.html"
+})
+
+controlBtn.addEventListener("click", () => {
+  if (controlActive == true) {
+    changeBlock("Login")
+    // Change alues
+    controlActive = false
+  } else if (controlActive == false) {
+    changeBlock("Cadastro")
+    // Change values
+    controlActive = true
+  }
+})
+
+window.addEventListener("load", () => {
+  changeBlock("Login")
 })
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,9 +1,11 @@
 const offcanvasMenu = document.getElementById("offcanvasNavbar")
 const offcanvasBottom = document.getElementById("offcanvasBottom")
 
+// Bloquei ao scroll qunado menu esta aberto
 offcanvasMenu.addEventListener('shown.bs.offcanvas', () => {window.minhaVariavel = true})
 offcanvasBottom.addEventListener('shown.bs.offcanvas', () => {window.minhaVariavel = true})
 
+// Permite scroll prop menu
 offcanvasMenu.addEventListener('hidden.bs.offcanvas', () => {window.minhaVariavel = false})
 offcanvasBottom.addEventListener('hidden.bs.offcanvas', () => {window.minhaVariavel = false})
 
